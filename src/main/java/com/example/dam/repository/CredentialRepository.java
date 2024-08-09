2f8259b9bbc18c56bf79e4348b3cd9d7627c4ddf
Ly Gia Bao <69019508+baorlys@users.noreply.github.com>
2024-08-09 17:13:43 +0700

    update model, add Credential valid key

--- src/main/java/com/example/dam/repository/CredentialRepository.java
+++ src/main/java/com/example/dam/repository/CredentialRepository.java
@@ -2,8 +2,12 @@ package com.example.dam.repository;
 
 import com.example.dam.model.Credential;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface CredentialRepository extends JpaRepository<Credential, UUID> {
+
+    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN TRUE ELSE FALSE END FROM Credential c WHERE c.tenant.id = ?1 AND c.apiKey = ?2 AND c.apiSecret = ?3")
+    boolean exists(String tenantId, String apiKey, String secretKey);
 }
