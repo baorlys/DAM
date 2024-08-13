2283498e6fe4354f827a650929ae592477f64e86
nguyenvoankhoa <nguyenvoankhoa@gmail.com>
2024-08-13 11:00:21 +0700

    solve conflict

---
+++ src/main/java/com/example/dam/global/mapper/DamMapper.java
@@ -0,0 +1,9 @@
+package com.example.dam.global.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DamMapper extends ModelMapper {
+
+}
