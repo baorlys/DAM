820d51b1a845d910c1f9160b2a48fdedacfa4c8e
Ly Gia Bao <69019508+baorlys@users.noreply.github.com>
2024-08-13 10:01:14 +0700

    fix mapper

--- src/main/java/com/example/dam/global/mapper/DamMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.dam.global.mapper;
-
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Component;
-
-@Component
-public class DamMapper extends ModelMapper {
-}
